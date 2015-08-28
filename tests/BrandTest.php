d2a898f8a375bf4419ec4a9cd54be1c325bdea0b
micahcourey <micahjcourey@gmail.com>
2015-08-28 11:55:24 -0700

    add Brand class and test file and pass GetName test

---
+++ tests/BrandTest.php
@@ -0,0 +1,38 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Brand.php";
+    require_once "src/Store.php";
+
+    $server = 'mysql:host=localhost:8889;dbname=shoe_store_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class BrandTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Brand::deleteAll();
+            Store::deleteAll();
+        }
+
+        function testGetName()
+        {
+            //Arrange
+            $name = 'Gucci';
+            $id = null;
+            $test_brand = new Brand($name, $id);
+
+            //Act
+            $result = $test_brand->getId();
+
+            //Assert
+            $this->assertEquals($id, $result);
+        }
+    }
+
+?>        
